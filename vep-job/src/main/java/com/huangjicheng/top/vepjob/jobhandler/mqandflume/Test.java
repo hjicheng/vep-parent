6dc7dfc6915f55cb2ecdf6a7bb04f306487a913c
huangjicheng <17335656950@163.com>
2020-08-11 13:58:25 +0800

    job 恢复

--- vep-job/src/main/java/com/huangjicheng/top/vepjob/jobhandler/mqandflume/Test.java
+++ vep-job/src/main/java/com/huangjicheng/top/vepjob/jobhandler/mqandflume/Test.java
@@ -5,7 +5,11 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.MessageProperties;
 
+import java.util.Random;
+
 public class Test {
+
+    public static final String[] strings = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
 
     // 生产消息 直连模式 rabbitmq 发送消息到flume 写入hdfs
     public static void sendMsg() throws Exception {
@@ -20,15 +24,16 @@ public class Test {
         // 参数3：是否独占队列,其他的channel/connect不能使用,一般false
         // 参数4：是否在消费完成后，自动删除队列【当消费者完全断开链接】 true不删除 false
         // 参数5：额外参数
-        channel.queueDeclare("user_log", true, false, false, null);
+        channel.queueDeclare("log_type", true, false, false, null);
 
         // 发布消息
         // 参数1：交换机名称
         // 参数2：队列名称
         // 参数3：额外参数 MessageProperties.PERSISTENT_TEXT_PLAIN 持久化
         // 参数4：内容
-        for (int i = 0; i < 10000000; i++) {
-            channel.basicPublish("", "user_log", MessageProperties.PERSISTENT_TEXT_PLAIN, ("用户" + i + "登录成功").getBytes());
+        for (int i = 0; i < 1000000; i++) {
+
+            channel.basicPublish("", "log_type", MessageProperties.PERSISTENT_TEXT_PLAIN, ("用户" + i + "登录成功," + strings[getRandom(0, 4)]).getBytes());
         }
 
         Util.closeConnection(channel, connection);
@@ -42,6 +47,24 @@ public class Test {
         } catch (Exception e) {
             e.printStackTrace();
         }
+//        for (int i = 0; i < 10000; i++) {
+//            System.out.println(strings[getRandom(0, 4)]);
+//        }
+
+
+    }
+
+    /**
+     * [min , max) 随机数
+     *
+     * @param min
+     * @param max
+     * @return
+     */
+    public static int getRandom(int min, int max) {
+        Random random = new Random();
+        int s = random.nextInt(max) % (max - min + 1) + min;
+        return s;
 
     }
 
